13eaad4d4f7062c9fbea8500e3f118478b6aa2d2
myselfmeii <bsmeiyarasu@gmail.com>
2023-08-28 18:24:58 +0530

    its helloWorld 28

---
+++ labs/corejava/helloWorld28/src/helloWorld28/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloWorld28;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 28");
+
+	}
+
+}
